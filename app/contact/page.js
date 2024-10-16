9b32a416c6a960a062d371ce0c7f64f0049e4427
lucaspeterc <lucasciolkowski@gmail.com>
2024-10-16 19:54:39 +0200

    sss

--- app/contact/page.js
+++ app/contact/page.js
@@ -202,7 +202,7 @@ export default function Contact () {
             type="submit"
             className="block w-full rounded-md bg-yellow-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-yellow-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-yellow-600"
           >
-            Let's talk
+            Let’s talk
           </button>
         </div>
       </form>
