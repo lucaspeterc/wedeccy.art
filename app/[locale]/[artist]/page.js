d4e7fb05dc1c71a406b893284ddd073c4bd9c8c0
lucaspeterc <lucasciolkowski@gmail.com>
2024-10-17 16:55:21 +0200

    sss

--- app/[locale]/[artist]/page.js
+++ app/[locale]/[artist]/page.js
@@ -1,10 +1,10 @@
 "use client";
 
 import { useParams } from 'next/navigation'; // Correct hook to use for dynamic route parameters in Next.js 14
-import { Navbar } from '/app/components/Navbar';
-import Hero from '/app/components/Hero';
-import Footer from '/app/components/Footer';
-import Banner from '/app/components/Banner';
+import { Navbar } from '/app/[locale]/components/Navbar';
+import Hero from '/app/[locale]/components/Hero';
+import Footer from '/app/[locale]/components/Footer';
+import Banner from '/app/[locale]/components/Banner';
 
 export default function ArtistPage() {
   const { artist } = useParams();  // Extract the 'artist' parameter from the dynamic route
---
+++ i18nConfig.js
@@ -0,0 +1,6 @@
+const i18nConfig = {
+    locales: ['en', 'de', 'pl'],
+    defaultLocale: 'pl'
+  };
+  
+  module.exports = i18nConfig;
