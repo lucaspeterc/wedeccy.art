e85e98d1f71287de0269f924eb064c9860120770
lucaspeterc <lucasciolkowski@gmail.com>
2024-10-21 21:16:32 +0200

    asas

--- app/[locale]/[artist]/contact/page.js
+++ app/[locale]/[artist]/contact/page.js
@@ -22,7 +22,7 @@ export default async function Contact({ params }) {
         {/* Pass the artist and locale explicitly to the Navbar */}
         <Navbar artist={artist} locale={locale} />
 
-        <div className="py-24 sm:py-36">
+        <div className="py-60 sm:py-44">
           <div className="mx-auto max-w-7xl px-6 lg:px-8">
             <div className="mx-auto max-w-2xl space-y-16 divide-y divide-gray-100 lg:mx-0 lg:max-w-none">
               <div className="grid grid-cols-1 gap-x-8 gap-y-10 lg:grid-cols-3">
--- app/[locale]/components/Exhibitions.js
+++ app/[locale]/components/Exhibitions.js
@@ -54,7 +54,7 @@ export default async function Exhibitions({ locale, artist }) {
   ];
 
   return (
-    <section className="py-36">
+    <section className="py-60">
       {/* Wanda's Exhibitions */}
       {artist === 'wanda' && (
         <>
