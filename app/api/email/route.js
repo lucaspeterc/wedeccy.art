8251dcc3ecf3bb15eabe0c1be1acfba838dae42d
lucaspeterc <lucasciolkowski@gmail.com>
2024-10-24 14:26:42 +0200

    hb

--- app/api/email/route.js
+++ app/api/email/route.js
@@ -1,45 +1,45 @@
-import mailgun from 'mailgun-js';
+// import mailgun from 'mailgun-js';
 
-// Make sure you import your environment variables
-const mg = mailgun({
-  apiKey: process.env.MAILGUN_API_KEY,
-  domain: process.env.MAILGUN_DOMAIN,
-});
+// // Make sure you import your environment variables
+// const mg = mailgun({
+//   apiKey: process.env.MAILGUN_API_KEY,
+//   domain: process.env.MAILGUN_DOMAIN,
+// });
 
-export async function POST(req) {
-  try {
-    const { firstName, lastName, email, phoneNumber, message } = await req.json();
+// export async function POST(req) {
+//   try {
+//     const { firstName, lastName, email, phoneNumber, message } = await req.json();
 
-    if (!email || !message) {
-      return new Response(
-        JSON.stringify({ error: 'Email and message are required' }),
-        { status: 400 }
-      );
-    }
+//     if (!email || !message) {
+//       return new Response(
+//         JSON.stringify({ error: 'Email and message are required' }),
+//         { status: 400 }
+//       );
+//     }
 
-    const data = {
-      from: 'Your Website <no-reply@yourdomain.com>', // This can be the sender email set up in Mailgun
-      to: process.env.CONTACT_RECEIVER_EMAIL, // Your email address where you want to receive the contact form messages
-      subject: 'New Contact Form Submission',
-      text: `
-        Name: ${firstName} ${lastName}
-        Email: ${email}
-        Phone: ${phoneNumber}
-        Message: ${message}
-      `,
-    };
+//     const data = {
+//       from: 'Your Website <no-reply@yourdomain.com>', // This can be the sender email set up in Mailgun
+//       to: process.env.CONTACT_RECEIVER_EMAIL, // Your email address where you want to receive the contact form messages
+//       subject: 'New Contact Form Submission',
+//       text: `
+//         Name: ${firstName} ${lastName}
+//         Email: ${email}
+//         Phone: ${phoneNumber}
+//         Message: ${message}
+//       `,
+//     };
 
-    const response = await mg.messages().send(data);
+//     const response = await mg.messages().send(data);
 
-    return new Response(
-      JSON.stringify({ message: 'Email sent successfully', response }),
-      { status: 200 }
-    );
-  } catch (error) {
-    console.error('Error sending email:', error);
-    return new Response(
-      JSON.stringify({ error: 'Error sending email' }),
-      { status: 500 }
-    );
-  }
-}
+//     return new Response(
+//       JSON.stringify({ message: 'Email sent successfully', response }),
+//       { status: 200 }
+//     );
+//   } catch (error) {
+//     console.error('Error sending email:', error);
+//     return new Response(
+//       JSON.stringify({ error: 'Error sending email' }),
+//       { status: 500 }
+//     );
+//   }
+// }
